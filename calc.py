44964a1b52152aeaa201d5fe65b57972c9f6d763
engels <engels.franca@icloud.com>
2025-05-01 13:56:07 -0300

    estilo e documentacao corrigidos

--- calc.py
+++ calc.py
@@ -1,6 +1,11 @@
+"""Modulos de uma calculadora."""
+
+
 def soma(x, y):
+    """Função que soma dois numeros."""
     return x + y
 
 
 def sub(x, y):
+    """Função que subratrai dois numeros."""
     return x - y
